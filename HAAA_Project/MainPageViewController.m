25cf2817f60b0d57060da08f91b2d5e0b636d3d2
amberlu00 <amberlu00@gmail.com>
2018-10-23 01:37:00 -0500

    Made main page interface

--- HAAA_Project/MainPageViewController.m
+++ HAAA_Project/MainPageViewController.m
@@ -8,7 +8,13 @@
 
 #import "MainPageViewController.h"
 
-@interface MainPageViewController ()
+@interface MainPageViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (strong, nonatomic) UITableView *mainNewTableView;
+
+@property (strong, nonatomic) AVPlayerViewController *mainPageVideoController;
+
+@property(strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *textLabel;
 
 @end
 
@@ -16,19 +22,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //Initializing important variables
+    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+    self.mainPageVideoController = [[AVPlayerViewController alloc] init];
+    self.mainPageVideoController.view.frame = CGRectMake(0, 60, CGRectGetWidth(screenBounds), 230);
+    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 300, CGRectGetWidth(screenBounds), 50)];
+    self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 355, CGRectGetWidth(screenBounds), 70)];
+    self.mainNewTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 440, CGRectGetWidth(screenBounds), CGRectGetHeight(screenBounds))];
     //Setting up the video for the main page
     NSString *mainPageVideoPath = [[NSBundle mainBundle] pathForResource:@"MainPageVideo" ofType:@"mp4"];
     NSURL *mainPageVideoURL = [NSURL fileURLWithPath:mainPageVideoPath];
-    AVPlayerViewController *mainPageVideoController = [[AVPlayerViewController alloc] init];
-    [self addChildViewController:mainPageVideoController];
-    [self.view addSubview:mainPageVideoController.view];
-    mainPageVideoController.view.frame = CGRectMake(0, 60, CGRectGetWidth([[UIScreen mainScreen] bounds]), 300);
+    [self addChildViewController:self.mainPageVideoController];
+    [self.view addSubview:self.mainPageVideoController.view];
     AVPlayer *mainPageVideoPlayer = [[AVPlayer alloc] initWithURL:mainPageVideoURL];
-    [mainPageVideoController setPlayer:mainPageVideoPlayer];
-    
+    [self.mainPageVideoController setPlayer:mainPageVideoPlayer];
     [mainPageVideoPlayer pause];
     
+    //Setting up label on the main page
+    NSString *mainPageHeading = @"Welcome to \n the Houston Asian-American Archives";
+    NSString *mainPageText = @"The Houston Asian-American Archives is an award-winning digital record of Houston Asian-Americans with the aim of preserving their unique cultural legacy.";
+    self.titleLabel.text = mainPageHeading;
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    self.titleLabel.font = [UIFont systemFontOfSize:20];
+    self.titleLabel.numberOfLines = 0;
+    self.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    self.textLabel.text = mainPageText;
+    self.textLabel.font = [UIFont systemFontOfSize:14];
+    self.textLabel.numberOfLines = 0;
+    self.textLabel.textAlignment = NSTextAlignmentCenter;
+    self.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    [self.view addSubview:self.textLabel];
+    [self.view addSubview:self.titleLabel];
+    
+    //Setting up the "new interviews" table in the main page
+    self.mainNewTableView.dataSource = self;
+    self.mainNewTableView.delegate = self;
+    [self.view addSubview:self.mainNewTableView];
+    self.mainNewTableView.clipsToBounds = YES;
 }
+
+#pragma mark - Table Functions
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSArray<NSString *> *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return @[@"Newest interviews: "];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *newCellIdentifier = @"mainNewPageCell";
+    UITableViewCell *individualCell = [self.mainNewTableView dequeueReusableCellWithIdentifier:newCellIdentifier];
+    individualCell.textLabel.text = @"Temporary";
+    if (individualCell == nil) {
+        individualCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:newCellIdentifier];
+    }
+    return individualCell;
+}
+
 
 /*
 #pragma mark - Navigation
